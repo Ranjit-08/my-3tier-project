1913d74b319004814b4983b45c847991bdcecf35
RJ-08 <ranjitsamalarj@gmail.com>
2026-01-20 18:34:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
 
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/users', (req, res) => {
+db.query('SELECT * FROM users', (err, results) => {
+if (err) return res.status(500).json(err);
+res.json(results);
+});
+});
+
+
+app.post('/users', (req, res) => {
+const { name, email } = req.body;
+db.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err) => {
+if (err) return res.status(500).json(err);
+res.json({ message: 'User added successfully' });
+});
+});
+
+
+app.listen(3000, () => {
+console.log('Backend running on port 3000');
+});
